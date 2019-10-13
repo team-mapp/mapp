f747cc5ce227778a14e40f4a52bb4b8e4d80f2f6
NamHyun Gu <mnhan0403@gmail.com>
2019-10-13 12:28:58 +0900

    Add various view type recycler adapter

--- app/src/main/java/ac/smu/embedded/findingtaste/main/MainActivity.kt
+++ app/src/main/java/ac/smu/embedded/findingtaste/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package ac.smu.embedded.findingtaste.main
 
 import ac.smu.embedded.findingtaste.R
-import ac.smu.embedded.findingtaste.util.BaseRecyclerAdapter
+import ac.smu.embedded.findingtaste.util.TypedItem
 import ac.smu.embedded.findingtaste.util.getViewModelFactory
 import ac.smu.embedded.findingtaste.util.recyclerAdapter
 import ac.smu.embedded.findingtaste.util.showToast
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_header.view.*
 import kotlinx.android.synthetic.main.item_test.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,24 +24,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setupToast()
 
-        btn_toast.setOnClickListener {
-            viewModel.showToast("Toast button clicked")
-            (recycler_view.adapter as BaseRecyclerAdapter<String>).addItem("Hello")
-        }
+        val adapter =
+            recyclerAdapter(
+                mapOf(
+                    TYPE_HEADER to R.layout.item_header,
+                    TYPE_ITEM to R.layout.item_test
+                ), mutableListOf(
+                    TypedItem(TYPE_HEADER, "Test")
+                )
+            ) { view, item ->
+                if (item.type == TYPE_HEADER) {
+                    view.tv_header.text = item.item
+                } else if (item.type == TYPE_ITEM) {
+                    view.tv_title.text = item.item
+                }
+            }
 
         recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.adapter =
-            recyclerAdapter(
-                R.layout.item_test,
-                mutableListOf<String>()
-            ) { view, string ->
-                view.tv_title.text = string
-            }
+        recycler_view.adapter = adapter
+
+        btn_toast.setOnClickListener {
+            viewModel.showToast("Toast button clicked")
+            adapter.addItem(TypedItem(TYPE_ITEM, "Hello"))
+        }
     }
 
     private fun setupToast() {
         viewModel.toastText.observe(this, Observer {
             showToast(it, Toast.LENGTH_SHORT)
         })
+    }
+
+    companion object {
+        const val TYPE_HEADER = 100
+        const val TYPE_ITEM = 101
     }
 }
--- app/src/main/java/ac/smu/embedded/findingtaste/util/RecyclerAdapterUtil.kt
+++ app/src/main/java/ac/smu/embedded/findingtaste/util/RecyclerAdapterUtil.kt
@@ -6,25 +6,66 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
+data class TypedItem<T>(val type: Int, val item: T)
+
 fun <T> recyclerAdapter(
     @LayoutRes viewHolderLayout: Int,
     initItems: MutableList<T> = mutableListOf(),
     initializer: (View, T) -> Unit
 ): BaseRecyclerAdapter<T> = BaseRecyclerAdapter(viewHolderLayout, initItems, initializer)
 
+fun <T : TypedItem<*>> recyclerAdapter(
+    viewHolderLayouts: Map<Int, Int>,
+    initItems: MutableList<T> = mutableListOf(),
+    initializer: (View, T) -> Unit
+): BaseRecyclerAdapter<T> = BaseRecyclerAdapter(viewHolderLayouts, initItems, initializer)
+
 class BaseRecyclerAdapter<T>(
-    @LayoutRes private val viewHolderLayout: Int,
+    private val viewHolderLayouts: Map<Int, Int>,
     private val items: MutableList<T>,
     private val initializer: (View, T) -> Unit
 ) : RecyclerView.Adapter<BaseRecyclerAdapter.BaseViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
-        BaseViewHolder(LayoutInflater.from(parent.context).inflate(viewHolderLayout, parent, false))
+    
+    constructor(
+        viewHolderLayout: Int,
+        items: MutableList<T>,
+        initializer: (View, T) -> Unit
+    ) : this(
+        mapOf(SINGLE_TYPE_ITEM to viewHolderLayout),
+        items,
+        initializer
+    )
 
-    override fun getItemCount(): Int = items.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        val layoutId = viewHolderLayouts[viewType]
+
+        if (layoutId != null) {
+            return BaseViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    layoutId,
+                    parent,
+                    false
+                )
+            )
+        } else {
+            throw IllegalArgumentException("Layout id not mapped")
+        }
+    }
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         initializer(holder.itemView, items[position])
     }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = items[position]
+        return if (item is TypedItem<*>) {
+            item.type
+        } else {
+            SINGLE_TYPE_ITEM
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
 
     fun addItem(newItem: T) {
         items.add(newItem)
@@ -49,5 +90,8 @@ class BaseRecyclerAdapter<T>(
     }
 
     class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-}
 
+    companion object {
+        private const val SINGLE_TYPE_ITEM = -1
+    }
+}
