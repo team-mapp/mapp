9e9fa2c7687c7eaefa2b7f3824ad32dd32ab3324
NamHyun Gu <mnhan0403@gmail.com>
2019-11-02 00:30:31 +0900

    Add disk cache strategy to load image

--- app/src/main/java/ac/smu/embedded/mapp/util/ImageViewExt.kt
+++ app/src/main/java/ac/smu/embedded/mapp/util/ImageViewExt.kt
@@ -6,8 +6,10 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.storage.StorageReference
 
 /**
  * Firebase Cloud Storage 에서 이미지를 가져와 [ImageView]에 보여주는 확장 함수
@@ -29,10 +31,10 @@ fun ImageView.load(
     requestOptions: RequestOptions? = null,
     requestListener: RequestListener<Drawable>? = null
 ) {
-    val application = context.applicationContext as BaseApplication
-    val reference = application.storage.getReference(imageLocation)
+    var builder = Glide.with(this)
+        .load(fetchImageReference(context, imageLocation))
+        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
 
-    var builder = Glide.with(this).load(reference)
     if (requestOptions != null) {
         builder = builder.apply(requestOptions)
     }
@@ -62,10 +64,11 @@ fun ImageView.loadBitmap(
     requestOptions: RequestOptions? = null,
     requestListener: RequestListener<Bitmap>? = null
 ) {
-    val application = context.applicationContext as BaseApplication
-    val reference = application.storage.getReference(imageLocation)
+    var builder =
+        Glide.with(this).asBitmap()
+            .load(fetchImageReference(context, imageLocation))
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
 
-    var builder = Glide.with(this).asBitmap().load(reference)
     if (requestOptions != null) {
         builder = builder.apply(requestOptions)
     }
@@ -74,3 +77,6 @@ fun ImageView.loadBitmap(
     }
     builder.into(this)
 }
+
+private fun fetchImageReference(context: Context, imageLocation: String): StorageReference =
+    (context.applicationContext as BaseApplication).storage.getReference(imageLocation)
