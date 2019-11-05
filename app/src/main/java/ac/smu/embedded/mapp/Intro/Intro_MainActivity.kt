e3122046c9f34c963282c8d27e0669a3a10f980f
Leejiso.o <ljsgold2001@naver.com>
2019-11-05 14:16:59 +0900

    jisoo_Intro

---
+++ app/src/main/java/ac/smu/embedded/mapp/Intro/Intro_MainActivity.kt
@@ -0,0 +1,13 @@
+package ac.smu.embedded.mapp.Intro
+
+import ac.smu.embedded.mapp.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Intro_MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro__main)
+    }
+}
