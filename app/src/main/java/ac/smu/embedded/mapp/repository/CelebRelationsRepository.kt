fef8c0b36fd8020c51513f4166a85dd0da78895c
NamHyun Gu <mnhan0403@gmail.com>
2019-11-06 16:29:36 +0900

    Add restaurants field to celeb and program

--- app/src/main/java/ac/smu/embedded/mapp/repository/CelebRelationsRepository.kt
+++ app/src/main/java/ac/smu/embedded/mapp/repository/CelebRelationsRepository.kt
@@ -7,6 +7,7 @@ import ac.smu.embedded.mapp.util.map
 import androidx.lifecycle.LiveData
 import com.google.firebase.firestore.FirebaseFirestore
 
+@Deprecated("셀럽 데이터에 식당의 id를 포함하도록 바꿨음 삭제될 예정")
 interface CelebRelationsRepository {
 
     fun loadCelebRelation(celebDocumentId: String): LiveData<Resource<CelebRelation?>>
