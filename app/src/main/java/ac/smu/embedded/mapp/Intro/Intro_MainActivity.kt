e10f0c087016fdbece21074699197a3358648d87
Leejiso.o <ljsgold2001@naver.com>
2019-11-10 04:27:27 +0900

    ADD_splash_background/intro_no_action_bar/Add_listener_intro

--- app/src/main/java/ac/smu/embedded/mapp/Intro/Intro_MainActivity.kt
+++ app/src/main/java/ac/smu/embedded/mapp/Intro/Intro_MainActivity.kt
@@ -1,13 +1,26 @@
 package ac.smu.embedded.mapp.Intro
 
 import ac.smu.embedded.mapp.R
+import ac.smu.embedded.mapp.main.MainActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_intro__main.*
 
 class Intro_MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro__main)
+
+        textView4.setOnClickListener{
+            var intent = Intent(this,MainActivity::class.java )
+
+            startActivity(intent)
+        }
     }
+
+
+
 }
