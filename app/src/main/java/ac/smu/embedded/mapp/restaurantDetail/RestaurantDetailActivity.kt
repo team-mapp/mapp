5c6bc14b8a3f65e995e0ceb3dfc7cdc5cc4b36ae
gaeun <gaeun2937@gmail.com>
2019-11-26 18:37:25 +0900

    change review list

--- app/src/main/java/ac/smu/embedded/mapp/restaurantDetail/RestaurantDetailActivity.kt
+++ app/src/main/java/ac/smu/embedded/mapp/restaurantDetail/RestaurantDetailActivity.kt
@@ -2,21 +2,28 @@ package ac.smu.embedded.mapp.restaurantDetail
 
 import ac.smu.embedded.mapp.R
 import ac.smu.embedded.mapp.model.Review
+import ac.smu.embedded.mapp.review.ReviewViewActivity
+import ac.smu.embedded.mapp.review.ReviewWriteActivity
 import ac.smu.embedded.mapp.util.BaseRecyclerAdapter
+import ac.smu.embedded.mapp.util.load
 import ac.smu.embedded.mapp.util.loadStorage
 import ac.smu.embedded.mapp.util.recyclerAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.request.RequestOptions
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import kotlinx.android.synthetic.main.activity_restaurantdetail.*
+import kotlinx.android.synthetic.main.item_restaurant_review.*
 import kotlinx.android.synthetic.main.item_restaurant_review.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -30,7 +37,7 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
     // 뷰 모델 설정
     var naverMap: NaverMap? = null
     private val restaurantDetailViewModel: RestaurantDetailViewModel by viewModel()
-    private lateinit var adapter: BaseRecyclerAdapter<Review>
+    private lateinit var adapter: BaseRecyclerAdapter<ReviewWithUser>
     var isFavorite: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +46,13 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
         if (intent.hasExtra(DOCUMENT_ID)) {
             // 여기서 documentId -> restaurantId
             val documentId: String = intent.getStringExtra(DOCUMENT_ID)
+
+            floatingButton.setOnClickListener { view ->
+                val intent = Intent(this, ReviewWriteActivity::class.java).apply {
+                    putExtra(ReviewWriteActivity.EXTRA_RESTAURANT_ID, documentId)
+                }
+                startActivity(intent)
+            }
 
             initView()
             loadContents(documentId)
@@ -52,7 +66,7 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
                             R.color.card_filter_color
                         )
                     )
-                    restaurantDetailViewModel.removeFavorite(USER_ID, documentId)
+                    restaurantDetailViewModel.removeFavorite(documentId)
                 } else {
                     isFavorite = true
                     btn_favorite.setColorFilter(
@@ -61,14 +75,16 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
                             R.color.content_favorite_color
                         )
                     )
-                    restaurantDetailViewModel.addFavorite(USER_ID, documentId)
+                    restaurantDetailViewModel.addFavorite(documentId)
                 }
             }
+
         } else { // 존재하지 않는 경우
             throw UnsupportedOperationException(
                 "Not received intent data, required EXTRA_DATA_TYPE, EXTRA_DOCUMENT_ID"
             )
         }
+
     }
 
     override fun onStart() {
@@ -110,12 +126,31 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
             setDisplayShowTitleEnabled(false)
         }
 
-
         // adapter 설정
         adapter = recyclerAdapter(R.layout.item_restaurant_review) { _, view, value ->
-            view.tvUserName.text = "${value!!.userId}"
-            view.tvRecommendPoint.text = "${value.content.recommendPoint}"
-            view.tvServicePoint.text = "${value.content.servicePoint}"
+            val user = value.user
+            if (user != null) {
+                with (user) {
+                    if (profileImage != null) {
+                        if (isExternalProfile()) {
+                            view.ivUserProfile.load(profileImage, listOf(RequestOptions().circleCrop()))
+                        }  else {
+                            view.ivUserProfile.loadStorage(profileImage, listOf(RequestOptions().circleCrop()))
+                        }
+                    }
+                    view.tvUserName.text = "$displayName"
+                }
+
+            }
+            view.rbCostRecommendPoint.rating = value.review.content.recommendPoint!!.toFloat()
+            view.rbCostServicePoint.rating = value.review.content.servicePoint!!.toFloat()
+
+            view.setOnClickListener {
+                val intent = Intent(this, ReviewViewActivity::class.java).apply {
+                    putExtra(ReviewViewActivity.EXTRA_REVIEW_ID, value.review.documentId)
+                }
+                startActivity(intent)
+            }
         }
         rvReviewList.adapter = adapter
         rvReviewList.layoutManager = LinearLayoutManager(this)
@@ -128,6 +163,7 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
             ivRestaurantImage.loadStorage(it!!.image)
             tvAddressDetail.text = "${it.address}"
             tvCallNumber.text = "${it.phone}"
+
             updateProfile(it.name, it.image)
 
             // 지도표시
@@ -142,8 +178,9 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
             val cameraUpdate = CameraUpdate.scrollTo(LatLng(latitude, longitude))
             naverMap?.moveCamera(cameraUpdate)
 
-            restaurantDetailViewModel.loadFavorite(USER_ID, documentId)
+            restaurantDetailViewModel.loadFavorite(documentId)
         })
+
 
         // favorite 하트 버튼 색 변경
         restaurantDetailViewModel.favorite.observe(this, Observer {
@@ -162,10 +199,13 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
         })
 
         // review list 가져오기
-        restaurantDetailViewModel.review.observe(this, Observer {
-            adapter.submitList(it)
-
-
+        restaurantDetailViewModel.reviews.observe(this, Observer {
+            if (it != null) {
+                tvEmptyReview.visibility = View.GONE
+                adapter.submitList(it)
+            }else{
+                tvEmptyReview.visibility = View.VISIBLE
+            }
         })
 
         restaurantDetailViewModel.loadRestaurant(documentId)
@@ -188,6 +228,5 @@ class RestaurantDetailActivity : AppCompatActivity(R.layout.activity_restaurantd
 
     companion object {
         const val DOCUMENT_ID = "document_id"
-        const val USER_ID = "test"
     }
 }
--- app/src/main/java/ac/smu/embedded/mapp/restaurantDetail/RestaurantDetailViewModel.kt
+++ app/src/main/java/ac/smu/embedded/mapp/restaurantDetail/RestaurantDetailViewModel.kt
@@ -2,6 +2,7 @@ package ac.smu.embedded.mapp.restaurantDetail
 
 import ac.smu.embedded.mapp.model.Restaurant
 import ac.smu.embedded.mapp.model.Review
+import ac.smu.embedded.mapp.model.User
 import ac.smu.embedded.mapp.repository.FavoriteRepository
 import ac.smu.embedded.mapp.repository.RestaurantsRepository
 import ac.smu.embedded.mapp.repository.ReviewRepository
@@ -10,6 +11,7 @@ import ac.smu.embedded.mapp.util.StateViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class RestaurantDetailViewModel(
@@ -18,39 +20,57 @@ class RestaurantDetailViewModel(
     private val reviewRepository: ReviewRepository,
     private val userRepository: UserRepository
 ) : StateViewModel() {
-    private val _review = MutableLiveData<List<Review>?>()
-    val review: LiveData<List<Review>?> = _review
+
+    val reviews: LiveData<List<ReviewWithUser>?> = useState()
+
+    val user: LiveData<User?> = useState()
 
     val restaurant: LiveData<Restaurant?> = useState()
-
-    fun loadUser(documentId: String) = viewModelScope.launch {
-        userRepository.getUser()
-    }
 
     fun loadRestaurant(documentId: String) = viewModelScope.launch {
         setState(restaurant, restaurantRepository.loadRestaurant(documentId))
     }
 
     fun loadReview(documentId: String) = viewModelScope.launch {
-        _review.value = reviewRepository.loadReviews(documentId)
+        reviewRepository.loadReviewsSync(documentId).collect {
+            val currentUser = userRepository.getUser()
+            val reviewWithUsers = it?.map { review ->
+                val user = userRepository.getUser(review.userId)
+                ReviewWithUser(review, user, currentUser != null && currentUser.uid == user?.uid)
+            }
+            setState(reviews, reviewWithUsers)
+        }
     }
 
     private val _favorite = MutableLiveData<Boolean>(false)
     val favorite: LiveData<Boolean> = _favorite
 
     // 좋아요 상태 확인
-    fun loadFavorite(userId: String, documentId: String) = viewModelScope.launch {
-        val favorites = favoriteRepository.loadFavorites(userId)
-        if (favorites != null) {
-            for (element in favorites) {
-                _favorite.value = documentId == element.restaurantId
+    fun loadFavorite(documentId: String) = viewModelScope.launch {
+        val user = userRepository.getUser()
+        if (user != null) {
+            val favorites = favoriteRepository.loadFavorites(user.uid!!)
+            if (favorites != null) {
+                for (element in favorites) {
+                    _favorite.value = documentId == element.restaurantId
+                }
             }
         }
     }
 
-    fun addFavorite(userId: String, documentId: String) =
-        favoriteRepository.addFavorite(userId, documentId)
+    fun addFavorite(documentId: String) = viewModelScope.launch {
+        val user = userRepository.getUser()
+        if(user!=null){
+            favoriteRepository.addFavorite(user.uid!!, documentId)
+        }
+    }
 
-    fun removeFavorite(userId: String, documentId: String) =
-        favoriteRepository.removeFavorite(userId, documentId)
+
+    fun removeFavorite(documentId: String) = viewModelScope.launch {
+        val user = userRepository.getUser()
+        if(user != null){
+            favoriteRepository.removeFavorite(user.uid!!, documentId)
+        }
+    }
+
 }
